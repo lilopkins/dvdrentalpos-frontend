850336c7855b890279f49bbcd8e43aea9c2d77f2
Lily Hopkins <lily@hpkns.uk>
2022-06-21 10:18:16 +0100

    Added LoadingSpinner confirmation

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from 'react';
 import './LoadingSpinner.css';
 
 export default function LoadingSpinner() {
-    return <div className="loading-spinner"><div class="lds-dual-ring"></div></div>;
+    const [extra, setExtra] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => setExtra("Still loading..."), 5000);
+    }, []);
+
+    return <div className="loading-spinner"><div class="lds-dual-ring"></div><div>{extra}</div></div>;
 }
